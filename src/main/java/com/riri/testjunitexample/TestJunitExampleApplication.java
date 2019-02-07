ea516b686a8ab31b2aab76247741201b2609a25d
Riri <rekikhaile@gmail.com>
2019-02-07 17:02:08 -0500

    test junit initial

---
+++ src/main/java/com/riri/testjunitexample/TestJunitExampleApplication.java
@@ -0,0 +1,14 @@
+package com.riri.testjunitexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJunitExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJunitExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/riri/testjunitexample/HelloResourceTest.java
@@ -0,0 +1,59 @@
+package com.riri.testjunitexample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jayway.jsonpath.JsonPath;
+
+import ch.qos.logback.core.status.Status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class HelloResourceTest {
+
+	
+	private MockMvc mockMvc;
+	
+	@InjectMocks
+	private HelloResource helloResource;
+	
+	@Before
+	public void setup() throws Exception {
+		mockMvc = MockMvcBuilders.standaloneSetup(helloResource)
+				.build();
+	}
+	
+	@Test
+	public void testHelloWorld() throws Exception {
+		
+		mockMvc.perform(
+				MockMvcRequestBuilders.get("/hello")
+				)
+		
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.content().string("hello world"));
+		
+	}
+	
+	/**@Test
+	public void testHelloJson() throws Exception {
+		 mockMvc.perform(MockMvcRequestBuilders.get("/hello/json")
+				 .accept(MediaType.APPLICATION_JSON))
+		 		.andExpect(MockMvcResultMatchers.status().isOk())
+		 		.andExpect(JsonPath())
+		
+	}*/
+
+	
+	
+}
